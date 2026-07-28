9c6ef8d87baef466e5bb5a3a3cb45bc15ec41a52
Bojie Liu <bojie.liu@github.com>
2026-07-28 22:15:08 +0800

    Remove files accidentally committed from untracked

--- _run_profiled.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PROFILE_WARMUP=1
-echo Starting with PROFILE_WARMUP=1
-cd /d E:\ETF_Surge\backend
-python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
--- start_profiled.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PROFILE_WARMUP=1
-cd /d "%~dp0backend"
-python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
